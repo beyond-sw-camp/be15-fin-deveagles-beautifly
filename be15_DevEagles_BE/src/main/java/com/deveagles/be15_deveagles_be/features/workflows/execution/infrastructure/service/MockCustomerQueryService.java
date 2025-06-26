b4f95e9ade8e4aae1b50bbbea878181fa472762d
Yangha Park <64etuor@gmail.com>
2025-06-26 15:05:29 +0900

    chore: 관련 없는 파일인데 주석만 추가한 거라 임의로 커밋함 #286

--- be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/workflows/execution/infrastructure/service/MockCustomerQueryService.java
+++ be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/workflows/execution/infrastructure/service/MockCustomerQueryService.java
@@ -11,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+// TODO: 나중에 지울 파일임
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
