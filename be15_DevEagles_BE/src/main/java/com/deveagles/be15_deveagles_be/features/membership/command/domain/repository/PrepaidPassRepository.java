9ff86400e836c6ef764cfa5ac5e535db485a2812
gyeongmin03 <gyeongmin03@naver.com>
2025-06-27 17:29:14 +0900

    feat: 회원권 조회 기능 구현 #303 - 선불권 조회 기능 구현

--- be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/membership/command/domain/repository/PrepaidPassRepository.java
+++ be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/membership/command/domain/repository/PrepaidPassRepository.java
@@ -1,7 +1,10 @@
 package com.deveagles.be15_deveagles_be.features.membership.command.domain.repository;
 
 import com.deveagles.be15_deveagles_be.features.membership.command.domain.aggregate.PrepaidPass;
+import java.util.List;
 
 public interface PrepaidPassRepository {
   PrepaidPass save(PrepaidPass prepaidPass);
+
+  List<PrepaidPass> findAllByDeletedAtIsNull();
 }
---
+++ be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/membership/query/service/PrepaidPassQueryService.java
@@ -0,0 +1,9 @@
+package com.deveagles.be15_deveagles_be.features.membership.query.service;
+
+import com.deveagles.be15_deveagles_be.features.membership.query.dto.response.PrepaidPassResponse;
+import java.util.List;
+
+public interface PrepaidPassQueryService {
+
+  List<PrepaidPassResponse> getAllPrepaidPass();
+}
