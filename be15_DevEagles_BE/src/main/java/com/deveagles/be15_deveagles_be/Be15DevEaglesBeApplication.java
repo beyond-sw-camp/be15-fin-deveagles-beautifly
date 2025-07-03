cc49cf4c708ae0603e570a3fe60a7d425f95fe25
hwan1023 <qpzmrlaxoghks@naver.com>
2025-07-03 20:55:35 +0900

    feat: 예약-발송-기능-구현#349

--- be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/Be15DevEaglesBeApplication.java
+++ be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/Be15DevEaglesBeApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/*
+@EnableScheduling
+ 해당 어노테이션하면 계속 스케쥴링 돌아서 우선 주석 처리 -> 전송되는지는 확인
+*/
 public class Be15DevEaglesBeApplication {
 
   public static void main(String[] args) {
