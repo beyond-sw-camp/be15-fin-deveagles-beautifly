8faf2dc1d9b79714f4f27103b5b6d3611966cf3e
gyeongmin03 <gyeongmin03@naver.com>
2025-07-03 14:53:57 +0900

    refactor: 상품 조회 기능 수정 #351 - 상품 조회 기능 수정

--- be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/items/command/domain/repository/PrimaryItemRepository.java
+++ be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/items/command/domain/repository/PrimaryItemRepository.java
@@ -3,11 +3,14 @@ package com.deveagles.be15_deveagles_be.features.items.command.domain.repository
 import com.deveagles.be15_deveagles_be.features.items.command.domain.aggregate.PrimaryItem;
 import java.util.List;
 import java.util.Optional;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PrimaryItemRepository {
   PrimaryItem save(PrimaryItem primaryItem);
 
   Optional<PrimaryItem> findById(Long id);
 
-  List<PrimaryItem> findAllByDeletedAtIsNull();
+  @Query("SELECT p FROM PrimaryItem p WHERE p.shopId.shopId = :shopId AND p.deletedAt IS NULL")
+  List<PrimaryItem> findAllByShopId(@Param("shopId") Long shopId);
 }
