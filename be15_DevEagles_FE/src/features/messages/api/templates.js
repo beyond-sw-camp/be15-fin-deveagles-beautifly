c8c891c0148fbb2c3dd3f229087e88bb70fd1134
Yangha Park <64etuor@gmail.com>
2025-07-09 17:14:35 +0900

    feat: 템플릿 응답값 매핑 수정, url 변경 #428

--- be15_DevEagles_FE/src/features/messages/api/templates.js
+++ be15_DevEagles_FE/src/features/messages/api/templates.js
@@ -1,22 +1,15 @@
 import api from '@/plugins/axios.js';
+import { useAuthStore } from '@/store/auth.js';
 
-const BASE_URL = '/templates';
+const BASE_URL = '/message/templates';
 
 // TODO: 메시지 템플릿 조회 연동시키려고 임의로 간단하게 만듦, 나중에 수정하세요(김태환)
 class TemplatesAPI {
-  async getTemplates({ shopId, ...searchParams } = {}) {
-    const params = {
-      ...searchParams,
-    };
-    if (shopId) params.shopId = shopId;
+  async getTemplates(searchParams = {}) {
+    const authStore = useAuthStore();
+    const params = { ...searchParams, shopId: authStore.shopId };
     const response = await api.get(BASE_URL, { params });
-    return (response.data.data?.content || []).map(t => ({
-      id: t.id,
-      name: t.templateTitle || t.name,
-      type: t.type,
-      content: t.content,
-      createdAt: t.createdAt,
-    }));
+    return response.data.data;
   }
 }
 
