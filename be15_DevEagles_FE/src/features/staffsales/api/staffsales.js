4087c27889c388dd1821762b9671e8f16784d0af
nare20027 <nare20027@gmail.com>
2025-07-10 11:08:21 +0900

    feat : 직원 결산 기능 구현 - 직원 상세 결산 조회 #410 - 상세 결산 api 연동

--- be15_DevEagles_FE/src/features/staffsales/api/staffsales.js
+++ be15_DevEagles_FE/src/features/staffsales/api/staffsales.js
@@ -1,3 +1,5 @@
 import api from '@/plugins/axios.js';
 
 export const getStaffSales = params => api.get('/staff-sales', { params });
+
+export const getStaffDetailSales = params => api.get(`/staff-sales/details`, { params });
